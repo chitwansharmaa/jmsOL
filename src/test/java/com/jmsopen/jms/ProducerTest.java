221802307acebcec5409ee920a4d0fd8e0db0ddc
Chitwan Sharma <64715607+chitwansharmaa@users.noreply.github.com>
2021-08-18 11:06:20 -0400

    Add files via upload

---
+++ src/test/java/com/jmsopen/jms/ProducerTest.java
@@ -0,0 +1,55 @@
+package com.jmsopen.jms;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import javax.jms.JMSException;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ProducerTest {
+
+  private static Producer producer;
+  private static Consumer consumer;
+
+  @BeforeClass
+  public static void setUpBeforeClass() throws JMSException {
+    producer = new Producer();
+    producer.create("helloworld.q");
+
+    consumer = new Consumer();
+    consumer.create("helloworld.q");
+  }
+
+  @AfterClass
+  public static void tearDownAfterClass() throws JMSException {
+    producer.close();
+    consumer.close();
+  }
+
+  @Test
+  public void testGetGreeting() {
+    try {
+      producer.sendName("Chitwan", "Sharma");
+
+      String greeting = consumer.getGreeting(1000);
+      assertEquals("Hello Chitwan Sharma!", greeting);
+
+    } catch (JMSException e) {
+      fail("a JMS Exception occurred");
+    }
+  }
+
+  @Test
+  public void testNoGreeting() {
+    try {
+      String greeting = consumer.getGreeting(1000);
+      assertEquals("no greeting", greeting);
+
+    } catch (JMSException e) {
+      fail("a JMS Exception occurred");
+    }
+  }
+}
